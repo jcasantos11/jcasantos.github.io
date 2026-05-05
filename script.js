92ded278e623221c936d42a015c94d6b518c65ad
jcasantos11 <31979879+jcasantos11@users.noreply.github.com>
2026-05-05 13:35:30 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -17,60 +17,6 @@ cards.forEach(card => {
   observer.observe(card);
 });
 
-// const timelineCards = document.querySelectorAll(".timeline-card");
-
-// const featuredRole = document.getElementById("featuredRole");
-// const featuredDate = document.getElementById("featuredDate");
-// const featuredTitle = document.getElementById("featuredTitle");
-// const featuredCompany = document.getElementById("featuredCompany");
-// const featuredDescription = document.getElementById("featuredDescription");
-// const featuredTags = document.getElementById("featuredTags");
-
-// function updateFeaturedCard(card) {
-//   timelineCards.forEach(item => item.classList.remove("active"));
-//   card.classList.add("active");
-
-//   featuredRole.classList.add("is-changing");
-
-//   setTimeout(() => {
-//     featuredDate.textContent = card.dataset.date;
-//     featuredTitle.textContent = card.dataset.title;
-//     featuredCompany.textContent = card.dataset.company;
-//     featuredDescription.textContent = card.dataset.description;
-
-//     featuredTags.innerHTML = "";
-
-//     card.dataset.tags.split(",").forEach(tag => {
-//       const span = document.createElement("span");
-//       span.textContent = tag.trim();
-//       featuredTags.appendChild(span);
-//     });
-
-//     featuredRole.classList.remove("is-changing");
-//   }, 180);
-// }
-
-// timelineCards.forEach(card => {
-//   card.addEventListener("click", () => {
-//     updateFeaturedCard(card);
-//   });
-// });
-
-// const observer2 = new IntersectionObserver(
-//   entries => {
-//     entries.forEach(entry => {
-//       if (entry.isIntersecting) {
-//         updateFeaturedCard(entry.target);
-//       }
-//     });
-//   },
-//   {
-//     root: document.getElementById("timelineScroller"),
-//     threshold: 0.65
-//   }
-// );
-
-// timelineCards.forEach(card => observer2.observe(card));
 
 const slider = document.getElementById("experienceSlider");
 const cards2 = slider.querySelectorAll(".itunes-card");
