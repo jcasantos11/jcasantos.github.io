52273e9dd8845d6bb46ef52fce5f69741419d380
jcasantos11 <31979879+jcasantos11@users.noreply.github.com>
2026-05-05 12:30:34 +0800

    uploaded initial files

---
+++ script.js
@@ -0,0 +1,18 @@
+const cards = document.querySelectorAll(".service-card, .project-card");
+
+const observer = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.15
+  }
+);
+
+cards.forEach(card => {
+  observer.observe(card);
+});
